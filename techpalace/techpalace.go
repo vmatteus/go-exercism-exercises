a598654af5c956f965815407425ba30c6a109af9
Vinicius Matteus <vmatteus@gmail.com>
2024-08-29 21:34:26 -0300

    fix os exercios de string e de ifs

---
+++ techpalace/techpalace.go
@@ -0,0 +1,21 @@
+package techpalace
+
+import "strings"
+
+// WelcomeMessage returns a welcome message for the customer.
+func WelcomeMessage(customer string) string {
+	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
+}
+
+// AddBorder adds a border to a welcome message.
+func AddBorder(welcomeMsg string, numStarsPerLine int) string {
+	return strings.Repeat("*", numStarsPerLine) + "\n" + welcomeMsg + "\n" + strings.Repeat("*", numStarsPerLine)
+}
+
+// CleanupMessage cleans up an old marketing message.
+func CleanupMessage(oldMsg string) string {
+	oldMsg = strings.Replace(oldMsg, "*", "", -1)
+	oldMsg = strings.Trim(oldMsg, "\n")
+	oldMsg = strings.Trim(oldMsg, " ")
+	return strings.Replace(oldMsg, "*", "", -1)
+}
