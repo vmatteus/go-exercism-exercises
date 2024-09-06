8b63b19aedf8fa3aa2ffb358a9a6c39f4b14d755
Vinicius Matteus <vmatteus@gmail.com>
2024-09-05 21:18:27 -0300

    ajustei o exercicio pra usar o pacote fmt para a substituição

--- partyrobot/party_robot.go
+++ partyrobot/party_robot.go
@@ -1,9 +1,6 @@
 package partyrobot
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 // Welcome greets a person by name.
 func Welcome(name string) string {
@@ -12,13 +9,10 @@ func Welcome(name string) string {
 
 // HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
 func HappyBirthday(name string, age int) string {
-	return "Happy birthday " + name + "! You are now " + strconv.Itoa(age) + " years old!"
+	return fmt.Sprintf("Happy birthday %s! You are now %d years old!", name, age)
 }
 
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-	return "Welcome to my party, " + name + "!\n" +
-		"You have been assigned to table " + fmt.Sprintf("%03d", table) + ". Your table is " + direction + ", exactly " +
-		fmt.Sprintf("%.1f", distance) + " meters from here." + "\n" +
-		"You will be sitting next to " + neighbor + "."
+	return fmt.Sprintf("Welcome to my party, %s!\nYou have been assigned to table %03d. Your table is %s, exactly %.1f meters from here.\nYou will be sitting next to %s.", name, table, direction, distance, neighbor)
 }
